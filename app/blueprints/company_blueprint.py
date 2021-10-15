17b85fec95d749138f7fd48cd65cf0d6675ba6a9
Marcos Polegario <mviniciuspvieira@hotmail.com>
2021-10-15 10:37:37 -0300

    fix: import from company_blueprint in api_blueprint

--- app/blueprints/company_blueprint.py
+++ app/blueprints/company_blueprint.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from app.controllers.company_controller import create_company, login
 
 
-company_bp = Blueprint('company_bp', __name__, url_prefix='companies')
+bp = Blueprint('company_bp', __name__, url_prefix='companies')
 
-company_bp.post('')(create_company)
-company_bp.post('/login')(login)
+bp.post('')(create_company)
+bp.post('/login')(login)
