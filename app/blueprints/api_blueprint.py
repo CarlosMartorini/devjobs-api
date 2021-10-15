13bc4f51347569dbf29d60d78dbd8fb02c32caae
eduardofernandes2103 <eduardofernandes2103@gmail.com>
2021-10-15 11:57:30 -0300

    TechSkills Blueprint, controller and model

--- app/blueprints/api_blueprint.py
+++ app/blueprints/api_blueprint.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
-from . import user_blueprint
+from . import user_blueprint, tech_skills_blueprint
 
 api_bp = Blueprint('api_bp', __name__, url_prefix='/api')
 
 api_bp.register_blueprint(user_blueprint.bp)
+api_bp.register_blueprint(tech_skills_blueprint.bp)
--- app/configs/migration.py
+++ app/configs/migration.py
@@ -5,5 +5,6 @@ from flask_migrate import Migrate
 def init_app(app: Flask):
 
     from app.models.user_model import UserModel
+    from app.models.tech_skill_model import TechSkillModel
 
     Migrate(app, app.db)
